c97857851a6ba5f19972779b7c01dae0fe5985a4
Marcin Olszewski <marcin.olszewski@akmf.pl>
2020-06-20 15:28:00 +0200

    fix

--- src/main/StringPermutation.java
+++ src/main/StringPermutation.java
@@ -5,7 +5,6 @@ class StringPermutation {
 
   private Set<String> out = new TreeSet<>();
 
-
   public static void main(String[] args) {
     StringPermutation stringPermutation = new StringPermutation();
     stringPermutation.start();
@@ -16,6 +15,7 @@ class StringPermutation {
     permute(a, 0);
     System.out.println(out.size());
     System.out.println(out);
+
     out.clear();
 
     a = "ABCD";
@@ -23,24 +23,31 @@ class StringPermutation {
     System.out.println(out.size());
     System.out.println(out);
 
+    out.clear();
+
+    a = "ABCDE";
+    permute(a, 0);
+    System.out.println(out.size());
+    System.out.println(out);
   }
 
   private void permute(String word, int start) {
-    for (int i = start; i < word.length(); i++) {
-      word = swap(word, start, i);
-      permute(word, start + 1);
-      word = swap(word, i, start);
-      if (start + 1 == word.length())
-        out.add(word);
+    if (start + 1 == word.length()) {
+      out.add(word);
+    } else {
+      for (int i = start; i < word.length(); i++) {
+        String wordSwapped = swap(word, start, i);
+        permute(wordSwapped, start + 1);
+      }
     }
   }
 
   private String swap(String word, int startPos, int currentPos) {
-    StringBuilder s = new StringBuilder(word);
+    StringBuilder sb = new StringBuilder(word);
     char charAtStart = word.charAt(startPos);
     char charAtCurrent = word.charAt(currentPos);
-    s.setCharAt(startPos, charAtCurrent);
-    s.setCharAt(currentPos, charAtStart);
-    return s.toString();
+    sb.setCharAt(startPos, charAtCurrent);
+    sb.setCharAt(currentPos, charAtStart);
+    return sb.toString();
   }
 }
